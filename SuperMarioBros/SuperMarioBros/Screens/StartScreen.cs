e5f2c4271d077cdf33d4c768b00cd25393af3e2a
Lizell Trabal <ltrabal@student.clayton.edu>
2013-10-26 21:56:46 -0400

    add screen managers and start screen class

---
+++ SuperMarioBros/SuperMarioBros/Screens/StartScreen.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Shooter.ScreenManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Shooter.Screens
+{
+    class StartScreen : GameScreen
+    {
+        ContentManager content;
+        Texture2D background;
+
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+            background = content.Load<Texture2D>("backgrounds/mainMenu");
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
