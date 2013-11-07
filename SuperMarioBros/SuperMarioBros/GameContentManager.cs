17dbe7f42b2ecdb65caea876397af412be6cfbe2
Casey Scarborough <caseyscarborough@gmail.com>
2013-11-07 10:09:45 -0500

    Add sound to GamePlay screen.

--- SuperMarioBros/SuperMarioBros/GameContentManager.cs
+++ SuperMarioBros/SuperMarioBros/GameContentManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,14 @@ namespace SuperMarioBros
     {
         public static Texture2D SpritesTexture;
         public static Texture2D MainMenuLogo;
+        public static Texture2D Mario;
+        public static Song MainTheme;
 
         public static void Initialize(Game game) {
             SpritesTexture = game.Content.Load<Texture2D>("Sprites/supermariobros");
             MainMenuLogo = game.Content.Load<Texture2D>("Sprites/main_menu_logo");
+            Mario = game.Content.Load<Texture2D>("Sprites/supermariobros");
+            MainTheme = game.Content.Load<Song>("Sounds/main_theme");
         }
     }
 }
--- SuperMarioBros/SuperMarioBros/Screens/GameplayScreen.cs
+++ SuperMarioBros/SuperMarioBros/Screens/GameplayScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Media;
 
 using SuperMarioBros.ScreenManagers;
 
@@ -9,5 +10,37 @@ namespace SuperMarioBros.Screens
 {
     public class GameplayScreen : GameScreen
     {
+        Song mainTheme;
+
+        public GameplayScreen()
+        {
+
+        }
+
+        private void PlayMusic(Song song)
+        {
+            try
+            {
+                // Play the music and loop the song
+                MediaPlayer.Play(song);
+                MediaPlayer.Volume = 20;
+                MediaPlayer.IsRepeating = true;
+            }
+            catch { }
+        }
+
+        private void FadeOutMusic()
+        {
+            while (MediaPlayer.Volume > 0)
+                MediaPlayer.Volume--;
+            MediaPlayer.Stop();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            PlayMusic(GameContentManager.MainTheme);
+        }
+
     }
 }
